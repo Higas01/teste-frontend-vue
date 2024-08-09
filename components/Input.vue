9274def843dfdf0c2efd0be1f9d52f8bcf93bc06
Higas <higor.porangaba123@hotmail.com>
2024-08-09 16:04:10 -0300

    feat: login screen created and stylized

---
+++ components/Input.vue
@@ -0,0 +1,18 @@
+<script setup>
+const modelInput = defineModel();
+defineProps({
+    label: String,
+    type: {
+        type: String,
+        default: "text",
+        validator: value => ["email", "password"].includes(value), 
+    },
+    
+})
+</script>
+<template>
+    <div class="form-floating mb-5">
+        <input :type="type" class="form-control" :name="label" v-model="modelInput" placeholder="name@example.com">
+        <label :for="label" class="text-secondary">{{ label }}</label>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<script setup>
+</script>
+<template>
+<section class="vh-100 gradient-custom">
+  <form class="container py-5 h-100" @submit.prevent="handleSubmit">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="card bg-dark text-white" style="border-radius: 1rem;">
+          <div class="card-body p-5 text-center">
+              <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+              <p class="text-white-50 mb-5">Acesse nosso sistema com Email e Senha</p>
+              <Input type="email" label="email" v-model="teste" />
+              <Input type="password" label="password" v-model="teste2" />
+              <button data-mdb-button-init data-mdb-ripple-init class="btn btn-outline-light btn-lg px-5" type="submit">Entrar</button>
+              <div class="d-flex justify-content-center text-center mt-4 pt-1">
+                {{ teste }}
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </form>
+</section>
+</template>
